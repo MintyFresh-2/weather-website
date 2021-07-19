6edb112d9444c42988615321fd8d5af67e7cd6a4
Barnaby <bs663@cam.ac.uk>
2021-07-19 18:42:50 +0100

    Setup app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,16 @@ const errorMessage = document.getElementById("error");
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   forecastMessage.textContent = "Loading...";
-  fetch(`http://localhost:3000/weather?address=${addressInput.value}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          forecastMessage.textContent = "";
-          return (errorMessage.textContent = data.error);
-        }
-        errorMessage.textContent = "";
-        forecastMessage.textContent = `${data.locationName}, ${data.locationRegion}, ${data.locationCountry}:
+  fetch(`/weather?address=${addressInput.value}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        forecastMessage.textContent = "";
+        return (errorMessage.textContent = data.error);
+      }
+      errorMessage.textContent = "";
+      forecastMessage.textContent = `${data.locationName}, ${data.locationRegion}, ${data.locationCountry}:
         ${data.weatherDescription}. It is currently ${data.weatherTemp} degrees but it feels like ${data.weatherFeelsLike}. There is a ${data.weatherPrecipitation}% chance of precipitation. (As of ${data.observationTime})`;
-        console.log(data);
-      });
-    }
-  );
+      console.log(data);
+    });
+  });
 });
